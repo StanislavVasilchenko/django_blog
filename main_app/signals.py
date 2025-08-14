aa7f2ecb8e20044858391abaa8b5e6cbecc9dfab
stanislav <stanislav.vasilchenko@yandex.ru>
2025-08-14 12:41:18 +0300

    add celery

--- main_app/signals.py
+++ main_app/signals.py
@@ -5,18 +5,11 @@ from django.urls import reverse
 
 from django_blog import settings
 from main_app.models import User
+from main_app.tasks import send_verification_email
 
 
 @receiver(post_save, sender=User)
 def user_update(sender, instance, *args, **kwargs):
 
     if not instance.is_verified:
-        send_mail(
-            subject='Verify your account',
-            message='Follow this link to verify your account: '
-            'http://localhost:8000%s' % reverse(
-                'main_app:verify', kwargs={'uuid': str(instance.verification_uuid)}),
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[instance.email],
-            fail_silently=False,
-        )
+        send_verification_email.delay(instance.id)
